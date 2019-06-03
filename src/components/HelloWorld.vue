a4c11aeef02dd5e022d845ada4d349f130ef5a96
yanglibo <yanglibo@jiuzhoutech.com>
2019-06-03 18:26:00 +0800

    add pub

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,15 @@
     <h1>{{ msg }}</h1>
     <br>
     <button v-on:click="showNotify">click here to show notify</button>
+    <div style="display:none;">
+      <a href="https://info.flagcounter.com/SCAF">
+        <img
+          src="https://s01.flagcounter.com/count2/SCAF/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_1/pageviews_1/flags_0/percent_0/"
+          alt="Flag Counter"
+          border="0"
+        >
+      </a>
+    </div>
   </div>
 </template>
 
